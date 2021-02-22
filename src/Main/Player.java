a8815aabef5d79ff8ece5ca580d78e41eb6d8324
YaseminTurkkan <turkkanyasemin@gmail.com>
2021-02-21 21:02:48 -0800

    Initial Player Class

---
+++ src/Main/Player.java
@@ -0,0 +1,36 @@
+package Main;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Player {
+    private String name;
+    private int lives;
+    ArrayList<Integer> scoreHistory;
+
+    Player(String givenName)
+    {
+        name = givenName;
+        lives = -1;
+        scoreHistory = new ArrayList<>();
+    }
+
+    public void setLives(int givenLives) {
+        lives = givenLives;
+    }
+
+    public int calculateHighScore() {
+        if (scoreHistory.size() != 0){
+            return Collections.max(scoreHistory);
+        }
+        return 0;
+    }
+
+    // Getters Below
+    public String getName() {
+        return name;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+}
