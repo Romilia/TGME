0b85580679b0fb5d7993722c87c1d92dc42a5c58
Andres Garcia Roman <agarciar@uci.edu>
2021-02-22 22:17:18 -0800

    Added Level class + Modified TurnManager class

---
+++ GameEngine/src/Main/Level.java
@@ -0,0 +1,23 @@
+package Main;
+
+import java.util.ArrayList;
+
+public class Level {
+    Board board;
+    int targetScore;
+    ArrayList<String> tiles;
+
+    Level(ArrayList<String> tiles){
+        this.tiles = tiles;
+    }
+
+    int getTargetScore(){
+        return targetScore;
+    }
+
+    // TODO: implement function
+    void generateNewBoard(){
+
+    }
+
+}
