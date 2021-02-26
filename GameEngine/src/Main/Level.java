7aa95bfb7cd356af5a8ab1044f74a6e1fc06eb0f
Andres Garcia Roman <agarciar@uci.edu>
2021-02-25 21:56:26 -0800

    Update on Level.java

--- GameEngine/src/Main/Level.java
+++ GameEngine/src/Main/Level.java
@@ -1,15 +1,20 @@
 package Main;
 
+import GameLogic.Move;
+
 import java.util.ArrayList;
 
 public class Level {
     Board board;
     int targetScore;
     ArrayList<String> tiles;
+    Move move;
 
-    Level(ArrayList<String> tiles, int row, int col){
+    Level(ArrayList<String> tiles, int row, int col, int target){
+        this.targetScore = target;
         this.tiles = tiles;
         board = new Board(tiles, row, col);
+        move = new Move(board);
     }
 
     int getTargetScore(){
